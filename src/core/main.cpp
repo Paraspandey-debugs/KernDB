1b1b41b996439d3ef0e9963ba3277e3102a2e784
Paraspandey-debugs <p.pandey250806@gmail.com>
2026-03-21 14:04:35 +0530

    feat(core) : added statement handling

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,11 +2,27 @@
 #include <string>
 #include <cstring>
 #include <sys/types.h>
+#include <algorithm>
+#include <cctype>
+
 typedef struct {
     std::string buffer;
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
 
 void print_prompt() { printf("db > "); }
 
@@ -29,20 +45,64 @@ void read_input(InputBuffer* input_buffer) {
 }
 
 void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer);
+    delete input_buffer;
 }
 
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+    if(input_buffer->buffer == ".exit"){
+        exit(EXIT_SUCCESS);
+    }else{
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer,Statement* statement){
+
+    std::string input = input_buffer->buffer;
+
+    // convert to uppercase
+    std::transform(input.begin(), input.end(), input.begin(),
+                [](unsigned char c) { return std::toupper(c); });
+
+    if (input.rfind("INSERT", 0) == 0) {
+        return PREPARE_SUCCESS;
+    }
+
+    if (input.rfind("SELECT", 0) == 0) {
+        return PREPARE_SUCCESS;
+    }
+        return PREPARE_UNRECOGNIZED_STATEMENT;
+
+}
+void execute_statement(Statement* statement){
+
+}
 int main(int argc , char* argv[]){
     InputBuffer* input_buffer = new_input_buffer();
     
     while(true){
         print_prompt();
         read_input(input_buffer);
-        if(input_buffer->buffer == ".exit"){
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }else{
-            printf("Unrecognized command '%s'. \n",input_buffer->buffer.c_str());
+        if(input_buffer->buffer[0] == '.'){
+            switch (do_meta_command(input_buffer)){
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s\n",input_buffer->buffer.c_str());
+                    continue;
+            }
         }
+        Statement statement;
+        switch(prepare_statement(input_buffer,&statement)){
+            case(PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s.\n",input_buffer->buffer.c_str());
+                continue;
+        }
+        execute_statement(&statement);
+        printf("Executed.\n");
+            
     }
 }
